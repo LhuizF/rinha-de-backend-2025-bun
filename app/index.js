f17c45fe6c5103fd8d1545d20fbdf8713a682ba1
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-07-13 21:24:24 -0300

    feat: add payment summary endpoint

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { paymentQueue, startWorker } from './worker.js';
+import database from './database.js';
 
 const app = express();
 app.use(express.json());
@@ -23,6 +24,62 @@ app.post('/payments', (req, res) => {
   paymentQueue.push(payment);
 
   res.status(202).json({ status: 'ok' });
+});
+
+app.get('/payments-summary', async (req, res) => {
+  const { from, to } = req.query;
+
+  try {
+    let query = `
+            SELECT
+                processor,
+                COUNT(*) AS "totalRequests",
+                SUM(amount) AS "totalAmount"
+            FROM
+                processed_payments
+        `;
+
+    const params = [];
+    if (from && to) {
+      query += ' WHERE processed_at BETWEEN $1 AND $2';
+      params.push(from, to);
+    } else if (from) {
+      query += ' WHERE processed_at >= $1';
+      params.push(from);
+    } else if (to) {
+      query += ' WHERE processed_at <= $1';
+      params.push(to);
+    }
+
+    query += ' GROUP BY processor;';
+
+    const { rows } = await database.query(query, params);
+
+    const summary = {
+      default: {
+        totalRequests: 0,
+        totalAmount: 0
+      },
+      fallback: {
+        totalRequests: 0,
+        totalAmount: 0
+      }
+    };
+
+    for (const row of rows) {
+      if (row.processor === 'default' || row.processor === 'fallback') {
+
+        summary[row.processor].totalRequests = parseInt(row.totalRequests, 10);
+        summary[row.processor].totalAmount = parseFloat(row.totalAmount) / 100;
+      }
+    }
+
+    res.status(200).json(summary);
+
+  } catch (error) {
+
+    res.status(500).json({ error: 'Failed to fetch payment summary.' });
+  }
 });
 
 const PORT = process.env.PORT || 3333;
