0e823fdbedb394cc7125ff6d4f6e8c97dcea47cb
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-07-13 21:23:29 -0300

    refactor: column naming conventions in processed_payments table

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE processed_payments  (
     id SERIAL PRIMARY KEY,
-    correlationId UUID NOT NULL UNIQUE,
+    correlation_id UUID NOT NULL UNIQUE,
     amount BIGINT NOT NULL,
     processor VARCHAR(10) NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    processed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX IF NOT EXISTS idx_created_at ON processed_payments(createdAt);
+CREATE INDEX IF NOT EXISTS idx_created_at ON processed_payments(processed_at);
